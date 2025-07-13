49097d56a0bee5201ac07ca639a46cd986f17bc7
Tessa Marelic <tessamarelic@gmail.com>
2025-07-13 16:44:11 +1000

    add codes

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,19 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+
+    // github.getOctokit = require('@actions/github').getOctokit; use to access github apis
+    // 1. Get input values
+    const bucket = core.getInput('bucket', { required: true });
+    const region = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    
+    // 2. Upload files to S3
+    const s3Uri = `s3://${bucket}`;
+    AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`);
+
     core.notice('Hello from custom javascript action!');
 }
 
